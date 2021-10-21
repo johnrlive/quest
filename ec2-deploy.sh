b88825fcbcf3e7a2cc7512e8e5bd72531638a9f2
johnrlive <jr@jrs-MacBook-Air.local>
2021-10-20 20:41:26 -0400

    remove restarting shell

--- ec2-deploy.sh
+++ ec2-deploy.sh
@@ -19,7 +19,6 @@ sudo chkconfig docker on
 echo "========================== INSTALL - Docker Compose ====="
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
-exec $SHELL
 docker-compose version
 
 
